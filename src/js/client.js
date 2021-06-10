51b06348fa43e3261528d61a9cbc899ed071dc8d
Clare Sweeny <clareesweeny@gmail.com>
2021-06-10 15:01:45 -0400

    Working Victory Graph

--- src/js/client.js
+++ src/js/client.js
@@ -2,72 +2,70 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
 
-const data2012 = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000}
+const dataWeek1 = [
+  {day: 1, hours: 8},
+  {day: 2, hours: 5},
+  {day: 3, hours: 7},
+  {day: 4, hours: 9},
+  {day: 5, hours: 8},
+  {day: 6, hours: 6},
+  {day: 7, hours: 8}
 ];
 
-const data2013 = [
-  {quarter: 1, earnings: 15000},
-  {quarter: 2, earnings: 12500},
-  {quarter: 3, earnings: 19500},
-  {quarter: 4, earnings: 13000}
+const dataWeek2 = [
+  {day: 1, hours: 2},
+  {day: 2, hours: 4},
+  {day: 3, hours: 6},
+  {day: 4, hours: 8},
+  {day: 5, hours: 6},
+  {day: 6, hours: 6},
+  {day: 7, hours: 8}
 ];
 
-const data2014 = [
-  {quarter: 1, earnings: 11500},
-  {quarter: 2, earnings: 13250},
-  {quarter: 3, earnings: 20000},
-  {quarter: 4, earnings: 15500}
-];
-
-const data2015 = [
-  {quarter: 1, earnings: 18000},
-  {quarter: 2, earnings: 13250},
-  {quarter: 3, earnings: 15000},
-  {quarter: 4, earnings: 12000}
+const dataWeek3 = [
+  {day: 1, hours: 5},
+  {day: 2, hours: 6},
+  {day: 3, hours: 7},
+  {day: 4, hours: 8},
+  {day: 5, hours: 8},
+  {day: 6, hours: 7},
+  {day: 7, hours: 6}
 ];
 
 class Main extends React.Component {
   render() {
     return (
       <div>
-        <h1>Victory Tutorial</h1>
+        <h1>Sleep Tracker</h1>
         <VictoryChart
-          domainPadding={10}
           theme={VictoryTheme.material}
+          domainPadding={20}
         >
-          <VictoryAxis
-            tickValues={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+          <VictoryAxis 
+            tickValues={[1, 2, 3, 4, 5, 6, 7]}
+            tickFormat={["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]}
           />
-          <VictoryAxis
+          <VictoryAxis 
             dependentAxis
-            tickFormat={(x) => (`$${x / 1000}k`)}
+            tickFormat={(x) => (`${x}`)}
           />
           <VictoryStack
             colorScale={"warm"}
           >
-            <VictoryBar
-              data={data2012}
-              x={"quarter"}
-              y={"earnings"}
+            <VictoryBar 
+              data={dataWeek1}
+              x="day"
+              y="hours"
             />
-            <VictoryBar
-              data={data2013}
-              x={"quarter"}
-              y={"earnings"}
+            <VictoryBar 
+              data={dataWeek2}
+              x="day"
+              y="hours"
             />
-            <VictoryBar
-              data={data2014}
-              x={"quarter"}
-              y={"earnings"}
-            />
-            <VictoryBar
-              data={data2015}
-              x={"quarter"}
-              y={"earnings"}
+            <VictoryBar 
+              data={dataWeek3}
+              x="day"
+              y="hours"
             />
           </VictoryStack>
         </VictoryChart>
